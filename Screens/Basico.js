2b79465386ae9b604267d07869cd8f5150b22b21
LStitch <jesus.gomez18@tectijuana.edu.mx>
2021-12-09 21:15:53 -0800

    testo semi end

--- Screens/Basico.js
+++ Screens/Basico.js
@@ -31,44 +31,44 @@ export default function Basico() {
                         source={Summoners}
                         style={styles.wallpapers}
                 />
-                <Text style={styles.titulo}>Teoría de Líneas</Text>
+                <Text style={styles.titulo}>{"\n"}Teoría de Líneas{"\n"}</Text>
                 <Text style={styles.texto}>La intención de este apartado es fortalecer las competencias en línea. 
                     Como concepto general, el nivel en línea es mucho mayor al de las siguientes fases, 
                     debido a que es la fase con mayor juego y la primera en producirse, cuando el jugador está más “fresco”.
                 </Text>
                 <Text style={styles.texto}>Es importante focalizar en las competencias que se extraen “hacia afuera” o para la posterioridad 
                     de esta fase, así como las malas manías o errores que se generan en línea. La fase de líneas debe ser vista como un 
-                    instrumento o herramienta para el verdadero choque final.
+                    instrumento o herramienta para el verdadero choque final.{"\n"}
                 </Text>
                 <Text style={styles.texto}>
                 La estructura de esta sección será dividida en varias partes, las cuales considero Las Bases de la Línea. Con estos
                 tres conceptos claramente explicados, conseguiremos mejorar notoriamente, sobretodo en nuestra capacidad de concebir 
                 la línea. Veremos desde arriba el porqué de todas nuestras acciones y entenderemos el cauce determinado que el Metagame 
-                adopta.
+                adopta.{"\n"}
                 </Text>
                 <Text style={styles.titulo}>
                     Farmeo
                 </Text>
                 <Text style={styles.texto}>
-                Si nos referimos brevemente a la línea, quizás la primera idea que se les asome a muchos sea la de Farmear. Para muchos 
+                {"\n"}Si nos referimos brevemente a la línea, quizás la primera idea que se les asome a muchos sea la de Farmear. Para muchos 
                 incluso, es lo único esencial e importante de la fase de líneas.
-                <Text style={styles.negritas}> “No morir y Farmear”. </Text>
-                Cuántas veces habremos oído esa verdad. Y es cierto, no hay nada malo en esa frase. El problema llega cuando el jugador
+                <Text style={styles.negritas}>{"\n"}{"\n"}“No morir y Farmear”.{"\n"}</Text>
+                {"\n"}Cuántas veces habremos oído esa verdad. Y es cierto, no hay nada malo en esa frase. El problema llega cuando el jugador
                 se obceca en solo cumplir eso. Hace Tunnel Vision entorno a esa idea, y todo lo que ocurre en el mapa, en la partida, se 
                 vuelve ajeno a él. Cuantas hemos vivido:
-                <Text style={styles.negritas}> “Conseguir 100 Creeps más que el contrario y echar a perder la partida”. </Text>
+                <Text style={styles.negritas}>{"\n"}{"\n"}“Conseguir 100 Creeps más que el contrario y echar a perder la partida”.{"\n"}{"\n"}</Text>
                 El Modus Operandi de muchos individuos, otra vez mal guiados por entornos superiores o ajenos a ellos, como la diabólica 
                 LCS, que tanto daño puede llegar a hacer, se centra primaria y a veces únicamente, en Farmear. El hecho de cumplir esto a 
                 rajatabla, no se capitaliza en un beneficio directo e ineludible, sino en una limitación propia del juego, privándose uno 
                 a sí mismo de libertad, y enunciando un comando inviable en plenitud de casos. Que ninguno se alarme aquí. El Farm es 
                 necesario. El Farm es una herramienta. Pero nadie nunca ha ganado por Farmear un número exacto de Minions en el menor 
                 tiempo posible.
-                <Text style={styles.negritas}> “El Farmeo nunca es lo más importante” </Text>
-                Aunque muchos, de primeras, consideren esto una vil y despiadada falacia, más propia de un rey corrupto que de un guía, 
+                <Text style={styles.negritas}>{"\n"}{"\n"}“El Farmeo nunca es lo más importante”{"\n"}</Text>
+                {"\n"}Aunque muchos, de primeras, consideren esto una vil y despiadada falacia, más propia de un rey corrupto que de un guía, 
                 no se alteren, vamos a aclararlo.
                 </Text>
                 <Text style={styles.titulo}>
-                    Wardeo
+                    Wardeo{"\n"}
                 </Text>
                 <Text style={styles.texto}>
                 Dependiente siempre del Meta y la Season, el Wardeo es uno de los factores más determinantes de la línea. Y no hace 
@@ -77,7 +77,7 @@ export default function Basico() {
                 peligrosa (Más tarde se explicará sobre esto en la sección 3. Tradeo). A dicha posición la llamaremos, más tarde, 
                 Posición Inviable. El morir en un Gank es algo importante. No solo el enemigo de línea se beneficia, sino que el Jungler 
                 enemigo se refuerza también, y ahora será capaz de transmitir la ventaja al resto de líneas.
-                <Text style={styles.negritas}> “Morir en un Gank en Early es como entregar la primera ala de Dragón” </Text>
+                <Text style={styles.negritas}>{"\n"}{"\n"}“Morir en un Gank en Early es como entregar la primera ala de Dragón” </Text>
                 </Text>
             </ScrollView>
             
--- Screens/Intermedio.js
+++ Screens/Intermedio.js
@@ -7,7 +7,75 @@ const Intermedio = ({navigation}) => {
     return (
         <View style={styles.container}>
             <ScrollView>
-                <Text></Text>
+                <Text style={styles.titulo}>
+                    Tips Anti Tilteo Intenso{"\n"}
+                </Text>
+                <Text style={styles.texto}>
+                1º No juegues ranked durante un par de días ( semanas o meses, depende de tu estado de estrés ).{"\n"}
+                </Text>
+                <Text style={styles.texto}>
+                2º Busca divertirte con amigos, aunque tu seas chullenger koreano y tus amigos hierro 4 en NA, juega con ellos, 
+                suelen ser muy emocionantes las partidas.{"\n"}
+                </Text>
+                <Text style={styles.texto}>
+                3º Practica campeones DEL META para estar preparado. Es decir, si eres main mid, obviamente, los picks de ahora 
+                suelen ser Vex, Lux, Yone, etc... No te agarres Jhin en ranked first time en la promo de otro contra un Zed 
+                (experiencia personal :/). Simplemente hazte una lista de campeones a jugar por cada linea, póntela delante de 
+                la pantalla and chill.
+                </Text>
+                <Text style={styles.texto}>
+                4º Si incluso jugando estando bien mentalmente no te ayuda, la solución es simple, en la esquina superior derecha, 
+                hay una X, click y aceptar salir del juego. Es lo mejor que puedes hacer en ese momento, cerrar y jugar un poco a 
+                otra cosa, como si juegas a OSU, minecraft, etc, juega otra cosa para dejar de ser un men tilteado, porque cuando 
+                estamos tilted, solemos parecernos al niño que pegaba a los demás en la escuela.
+                </Text>
+                <Text style={styles.texto}>
+                5º Sigues sin salir del estado Tilted? Sal por ahí. Muchos (me incluyo) no tenemos una vida social plenamente activa, 
+                pero incluso yo salgo de vez en cuando, voy al cine, quedo con algún amigo o simplemente salgo a dar un paseo. La cosa 
+                es volver a sentirse bien con uno mismo y no dejar que el juego sea una fuente de estrés gratuita.{"\n"}
+                </Text>
+                <Text style={styles.texto}>
+                6º Deja de copiarles a los profesionales, tu no eres uno de ellos, si faker juega full ap Alistar mid con mejais 
+                TU NO LO HAGAS, NO ERES FAKER. Si solo intentas copiarlos nunca mejorarás , y te frustrarás más y aun más rápido. 
+                Los profesionales son una guía para nosotros, no una imagen a seguir ciegamente.{"\n"}
+                </Text>
+                <Text style={styles.texto}>
+                7º En el caso de que seas un testarudo y sigas jugando ranked sabiendo que estás tilted te recomiendo escoger siempre 
+                campeones del meta y que sepas jugar, no hagas experimentos, te saldrán mal. Y por encima de todo, o esconder el chat 
+                o simplemente ser constructivo, intentar hacer autocrítica y no enfocarte en flamear al que va 0-8 en mid contra una 
+                Leblanc o cosas del estilo.{"\n"}
+                </Text>
+                <Text style={styles.titulo}>
+                Que debo hacer para sacar una S+ con todos los campeones
+                </Text>
+                <Text style={styles.negritas}>
+                {"\n"}Puntos de visión
+                </Text>
+                <Text style={styles.texto}>
+                - Que tanto duro el ward{"\n"}
+                - Que tan cerca se coloco a la base enemiga{"\n"}
+                - A cuantos enemigos detecto o cuantos objetivos detecto{"\n"}
+                - Destruir wards enemigos{"\n"}
+                </Text>
+                <Text style={styles.negritas}>
+                Puntos de combate
+                </Text>
+                <Text style={styles.texto}>
+                - Participación en Kills{"\n"}
+                - KDA (Kill/Death/Assist){"\n"}
+                - Daño inflingido{"\n"}
+                - Mantener bajas tus muertes{"\n"}
+                </Text>
+                <Text style={styles.negritas}>
+                Puntos de oro
+                </Text>
+                <Text style={styles.texto}>
+                - Cuanto oro por minuto consigues{"\n"}
+                - Cuantos minions farmeaste{"\n"}
+                - Cuantos campeones asesinaste{"\n"}
+                - Objetivos hechos{"\n"}
+                </Text>
+                
             </ScrollView>
         </View>
     )
